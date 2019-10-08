f6b9f4164fb4cb76d244d03f55b0b070f538e199
Liubov Kaper <luba@Liubovs-MacBook-Pro.local>
2019-10-08 18:13:16 -0400

    ye

--- Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
+++ Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
@@ -8,11 +8,15 @@ import UIKit
 
 let numbers = [1,1,2,4,4,4,6,6,7,8]
 
-var numbersWithNoDuplicates = [Int]()
-
 // Your code here
 
-//assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
+var mySet = Set(numbers)
+
+var numbersWithNoDuplicates = [Int]()
+numbersWithNoDuplicates = Array(mySet.sorted())
+print(numbersWithNoDuplicates)
+
+assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
 
 // Questions Two
 
@@ -21,9 +25,18 @@ var numbersWithNoDuplicates = [Int]()
 let scores = [1, 77, 83, 32, 77, 77, 83, 32, 99]
 
 var scoresThatAppearOnce = [Int]()
-
+//var numCount = 0
+//for num in scores {
+//    if num != num {
+//        scoresThatAppearOnce.append(num)
+//    }
+//    print(scoresThatAppearOnce)
+//}
 // Your code here
-
+//var scoreSet = Set(scores)
+//var scoreArray = Array(scoreSet)
+//print(scores)
+//print(scoreArray)
 //assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
 
 // Question Three
@@ -35,11 +48,15 @@ var scoresThatAppearOnce = [Int]()
 let arrOne = [1,2,3,4,5]
 let arrTwo = [3,4,5,6,7]
 
-var arrThree: [Int] = []
+//var arrThree: [Int] = []
 
 // Your code here
-
-//assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
+var arrOneSet = Set(arrOne)
+var arrTwoSet = Set(arrTwo)
+var setUnion = arrOneSet.union(arrTwoSet).sorted()
+var arrThree = Array(setUnion)
+print(arrThree)
+assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
 
 // b.
 
@@ -48,11 +65,15 @@ var arrThree: [Int] = []
 let arrFour = [1,2,3,4,5]
 let arrFive = [3,4,5,6,7]
 
-var arrSix: [Int] = []
+// var arrSix: [Int] = []
 
 // Your code here
-
-//assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
+var arrFourSet = Set(arrFour)
+var arrFiveSet = Set(arrFive)
+var setIntersection = arrFourSet.intersection(arrFiveSet).sorted()
+var arrSix = Array(setIntersection)
+print(setIntersection)
+assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
 
 // Question Four
 
@@ -63,11 +84,17 @@ let numsTwo = [1, 2, 3, 4, 5, 6]
 let numsThree = [5, 6, 7, 8, 9, 10, 11, 12]
 let numsFour = [1, 3, 4, 5, 6, 7, 9]
 
-var allNumsWithNoDuplicates: [Int] = []
+//var allNumsWithNoDuplicates: [Int] = []
 
 // Your code here
-
-//assert(allNumsWithNoDuplicates == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "Was expecting [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], but got \([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])")
+let numsOneSet = Set(numsOne)
+let numsTwoSet = Set(numsTwo)
+let numsThreeSet = Set(numsThree)
+let numsFourSet = Set(numsFour)
+let unionOneTwo = numsOneSet.union(numsTwo).union(numsThree).union(numsFour).sorted()
+print(unionOneTwo)
+var allNumsWithNoDuplicates = Array(unionOneTwo)
+assert(allNumsWithNoDuplicates == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "Was expecting [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], but got \([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])")
 
 
 // Question Five
@@ -86,12 +113,38 @@ var strThreeIsPangram: Bool = false
 
 // Your code here
 
-//assert(strOneIsPangram == true, "Was expecting true, but got \(strOneIsPangram)")
-//assert(strTwoIsPangram == false, "Was expecting false, but got \(strTwoIsPangram)")
-//assert(strThreeIsPangram == true, "Was expecting true, but got \(strThreeIsPangram)")
+let alphabet: Set<Character> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+var strOneNoSpace = strOne.replacingOccurrences(of: " ", with: "")
+var strTwoNoSpace = strTwo.replacingOccurrences(of: " ", with: "")
+var strThreeNoSpace = strThree.replacingOccurrences(of: " ", with: "").replacingOccurrences(of: ",", with: "")
+var setCharactersStrOne = Set(strOneNoSpace.lowercased())
+var setCharactersStrTwo = Set(strTwoNoSpace.lowercased())
 
+var setCharactersStrThree = Set(strThreeNoSpace.lowercased())
+print(setCharactersStrThree.sorted())
 
+if alphabet == setCharactersStrOne {
+    strOneIsPangram = true
+} else {
+    strOneIsPangram = false
+}
+print(strOneIsPangram)
+if alphabet == setCharactersStrTwo {
+    strTwoIsPangram = true
+} else {
+    strTwoIsPangram = false
+}
+print(strTwoIsPangram)
+if alphabet == setCharactersStrThree {
+    strThreeIsPangram = true
+} else {
+    strThreeIsPangram = false
+}
+print(strThreeIsPangram)
+assert(strOneIsPangram == true, "Was expecting true, but got \(strOneIsPangram)")
+assert(strTwoIsPangram == false, "Was expecting false, but got \(strTwoIsPangram)")
+assert(strThreeIsPangram == true, "Was expecting false, but got \(strThreeIsPangram)")
 
 
 
